950b811941d547d319c0906e20f44d46f445b9b9
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-02-22 19:03:18 +0100

    added

--- src/js/app.js
+++ src/js/app.js
@@ -51,3 +51,44 @@ https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#
  * @example https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+
+
+
+ const resultContainer = document.querySelector(".results");
+
+ const url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
+ 
+ async function fetchPokemon () {
+   try {
+     const response = await fetch(url);
+     const json = await response.json();
+ 
+     console.log(json);
+ 
+     resultContainer.innerHTML = "";
+ 
+     const pokemons = json.results;
+ 
+     pokemons.forEach(async function (pokemon) {
+       const pokemonResponse = await fetch(pokemon.url);
+       const pokemonData = await pokemonResponse.json();
+       
+       resultContainer.innerHTML += `
+         <a href="index.html?id=${pokemonData.name}" class="card">
+           <div class="details">
+             <img src="${pokemonData.sprites.front_default}" class="image"/>  
+             <h4 class="name">${pokemonData.name}</h4> 
+           </div>
+         </a>
+       `;
+     });
+   } catch (error) {
+     console.log(error);
+     resultContainer.innerHTML = "Error: " + error.message;
+   }
+ }
+ 
+ fetchPokemon();
+ 
+
+
--- src/js/details.js
+++ src/js/details.js
@@ -44,3 +44,67 @@ Helper functions
  * @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+
+
+//  const queryString = document.location.search;
+//  const params = new URLSearchParams(queryString);
+//  const id = params.get("name");
+ 
+//  const url = `https://pokeapi.co/api/v2/pokemon?name=${id}`;
+ 
+//  async function fetchPokemon() {
+//    const response = await fetch(url);
+//    const json = await response.json();
+ 
+//    console.log(json);
+//  }
+ 
+//  fetchPokemon();
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+const pokemonName = document.querySelector(".title");
+const imageOfPokemon = document.querySelector(".image-pokemon");
+const abilityHtml = document.querySelector(".abilities");
+const statHtml = document.querySelector(".stats");
+
+async function fetchPokemonDetails() {
+  try {
+    const response = await fetch(url);
+    const pokemonDetails = await response.json();
+
+    showPokemonDetails(pokemonDetails);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function showPokemonDetails(pokemonDetails) {
+  console.log(pokemonDetails);
+  pokemonName.innerHTML = pokemonDetails.name;
+  imageOfPokemon.src = pokemonDetails.sprites.front_default;
+  for (let i = 0; i < pokemonDetails.abilities.length; i++) {
+    let ability = pokemonDetails.abilities[i].ability.name;
+    let abilityList = `<li>${ability}</li>`;
+    abilityHtml.innerHTML += abilityList;
+  }
+  for (let i = 0; i < pokemonDetails.stats.length; i++) {
+    let stat = pokemonDetails.stats[i].stat.name;
+    let baseStat = pokemonDetails.stats[i].base_stat;
+    let statList = `<li>${stat}: ${baseStat}</li>`;
+    statHtml.innerHTML += statList;
+  }
+}
+
+fetchPokemonDetails();
+
+
+
+
+ 
+ 
+
+
